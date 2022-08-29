f599a9c7460a478b4e7bfba6e0be2c3199276206
Nathan Shaw <n.shaw@entando.com>
2022-08-29 15:41:20 -0400

    SPBC-1 Add preset for tableau url

--- microfrontends/tableau-mfe-config/src/App.js
+++ microfrontends/tableau-mfe-config/src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      url: this.props.url
+      url: props.url || 'https://public.tableau.com/views/RegionalSampleWorkbook/Economy',
     };
   }
 
