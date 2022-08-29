da45c69828a21d5e91e92d0bb6cdef7e73417129
Nathan Shaw <n.shaw@entando.com>
2022-08-29 15:42:51 -0400

    Add test 2nd config widget freshly generated from create-react-app

---
+++ microfrontends/tableau-mfe-config2/src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: props.url || 'https://public.tableau.com/views/RegionalSampleWorkbook/Economy',
+    };
+  }
+
+  handleChange = e => {
+    const input = e.target;
+    this.setState({
+      [input.name]: input.value,
+    });
+  };
+
+  render() {
+    const { url } = this.state;
+    return (
+        <div>
+          <h1>Tableau Demo Configuration</h1>
+          <div>
+            <label htmlFor="url">Tableau View URL</label>
+            <input id="url" name="url" defaultValue={url} type="text" onChange={this.handleChange}  />
+          </div>
+        </div>
+    );
+  }
+}
+
+export default App;
